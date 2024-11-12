93ef78dbbd7028db4a646377a02c0cca89532a42
AstrologSecra <astrologworkmail@gmail.com>
2024-11-12 18:03:43 +0300

    Update art.sh

--- art.sh
+++ art.sh
@@ -2,7 +2,7 @@
 
 # Функция для вывода ASCII-арта
 print_kalibian_art() {
-    local art="
+    local art='''
     /$$   /$$  /$$$$$$  /$$       /$$$$$$ /$$$$$$$  /$$$$$$  /$$$$$$  /$$   /$$
     | $$  /$$/ /$$__  $$| $$      |_  $$_/| $$__  $$|_  $$_/ /$$__  $$| $$$ | $$
     | $$ /$$/ | $$  \ $$| $$        | $$  | $$  \ $$  | $$  | $$  \ $$| $$$$| $$
@@ -11,24 +11,23 @@ print_kalibian_art() {
     | $$\  $$ | $$  | $$| $$        | $$  | $$  \ $$  | $$  | $$  | $$| $$\  $$$
     | $$ \  $$| $$  | $$| $$$$$$$$ /$$$$$$| $$$$$$$/ /$$$$$$| $$  | $$| $$ \  $$
     |__/  \__/|__/  |__/|________/|______/|_______/ |______/|__/  |__/|__/  \__/
-    "
+    '''
 
     echo "$art"
 }
 
-# Функция для добавления ASCII-арта в .bashrc или .bash_profile
+# Функция для добавления ASCII-арта в .bashrc
 add_kalibian_art() {
     local profile_file="$HOME/.bashrc"
-    if [[ -f "$HOME/.bash_profile" ]]; then
-        profile_file="$HOME/.bash_profile"
-    fi
 
     # Проверяем, есть ли уже функция в файле
     if ! grep -q "print_kalibian_art" "$profile_file"; then
         echo "Функция print_kalibian_art не найдена в $profile_file. Добавляем..."
         echo -e "\n# Функция для вывода ASCII-арта" >> "$profile_file"
         echo -e "print_kalibian_art() {" >> "$profile_file"
-        echo -e "    local art=\"$art\"" >> "$profile_file"
+        echo -e "    local art='''" >> "$profile_file"
+        echo -e "    $art" >> "$profile_file"
+        echo -e "    '''" >> "$profile_file"
         echo -e "    echo \"\$art\"" >> "$profile_file"
         echo -e "}" >> "$profile_file"
         echo -e "\n# Вызов функции для вывода ASCII-арта" >> "$profile_file"
@@ -43,4 +42,4 @@ add_kalibian_art() {
 add_kalibian_art
 
 # Перезагрузка профиля для применения изменений
-source "$HOME/.bashrc" 2>/dev/null || source "$HOME/.bash_profile" 2>/dev/null
+source "$HOME/.bashrc" 2>/dev/null
